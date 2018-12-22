4ecbb88d13284ab5c843900ae1d5c662614cc42a
morgangrace11 <morgangrace720@gmail.com>
2018-12-22 18:45:28 -0500

    rendering on the front end

---
+++ client/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>are you here</div>
+    )
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  entry: './client/index.js',
+  entry: './client/index.jsx',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
