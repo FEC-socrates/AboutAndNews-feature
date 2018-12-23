73a5644ba322ce7fd05358add258c950dc90c932
morgangrace11 <morgangrace720@gmail.com>
2018-12-23 14:47:20 -0500

    front end get request complete

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      about: [],
+      news: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('/api/about')
+    .then(response => {
+      this.setState({
+        about: response.data
+      });
+    });
+
+    axios.get('/api/news')
+    .then(response => {
+      this.setState({
+        news: response.data
+      });
+    });
   }
 
   render() {
+    console.log('staaateee', this.state);
     return (
       <div>are you here</div>
     )
