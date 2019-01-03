a9531430e74307edf979ce95a0212c04b83d8506
morgangrace11 <morgangrace720@gmail.com>
2019-01-02 20:25:46 -0500

    completed testing

--- tests/news.test.js
+++ tests/news.test.js
@@ -1,43 +1,20 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import News from '../client/components/news.jsx';
-
-// describe('app tests', () => {
-
-//   const wrapper = mount(<App />);
-
-//   it('should render a <div />', () => {
-//     expect(wrapper.find('div').length).toEqual(8);
-//   });
-
-//   it('should render a classname about', () => {
-//     expect(wrapper.find('.aboutShow')).toBeDefined();
-//   });
-
-//   it('should render a show more button', () => {
-//     expect(wrapper.find(<a onClick={this.handleInfoClick}>Show Less</a>))
-//   })
-
-//   it('should render read more', () => {
-//     expect(wrapper.find('.aboutShow').text()).toEqual(' Show More')
-//   })
+import { fakeNewsData } from './jestsetup.js';
+import React from 'react';
 
 
-//    it('should render a show Less button', () => {
-//     wrapper.setState({
-//       open: true
-//     })
-//     expect(wrapper.find(
-//      <a onClick={this.handleInfoClick}>Show Less</a>))
-//   })
+describe('app tests', () => {
 
+  const wrapper = mount(<News />);
 
-//   it('should render a read Less', () => {
-//     wrapper.setState({
-//       open: true
-//     })
+   it('should render a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(2);
+  });
 
-//     expect(wrapper.find('.aboutShow').text()).toEqual(' Show Less')
-//   })
+  it('should have a class news', () => {
+    expect(wrapper.find('.news')).toBeDefined();
+  })
 
-// });
+});
